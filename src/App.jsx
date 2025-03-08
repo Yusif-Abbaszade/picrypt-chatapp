f7c3f47d6954c30a3cfbeb3cd913c4e9aec5150d
Yusif Abbaszade <yusif@yusif.yusif>
2025-03-08 06:10:05 -0500

    CHAT_WITH_SINGLE_PERSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react'
-import { supabase } from '../supabaseClient';
+import { supabase, supabaseAdmin } from '../supabaseClient';
 import { BsGoogle } from "react-icons/bs";
 import { IconContext } from 'react-icons/lib';
 import SignUp from './pages/SignUp';
@@ -10,6 +10,9 @@ const App = () => {
   const [session, setSession] = useState([]);
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
+  const [allUsers, setAllUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState('public');
+
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
@@ -25,6 +28,14 @@ const App = () => {
 
 
   }, []);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await supabaseAdmin.auth.admin.listUsers();
+      setAllUsers(data.users);
+    }
+    fetchUsers();
+  }, [])
 
 
   // sign in
@@ -60,17 +71,30 @@ const App = () => {
 
   //add message
   const addMessage = async (e) => {
+    
     await e.preventDefault();
+    if (newMessage === '') {
+      return
+    }
     await supabase
       .from('Messages')
       .insert([
-        { sender: session.user.email, message: newMessage, to: 'public' },
+        { sender: session.user.email, message: newMessage, to: selectedUser },
       ]);
 
     await fetchMessages();
     setNewMessage('');
     document.querySelector('.msgbox').value = '';
   }
+
+  const setSelectedClass = (id) => {
+    const allUsers = document.querySelectorAll('.all-users div');
+    allUsers.forEach((user) => {
+      user.classList.remove('selected');
+    })
+    document.getElementById(id).classList.add('selected');
+  }
+
   useEffect(() => {
     fetchMessages();
     supabase
@@ -88,6 +112,12 @@ const App = () => {
       )
       .subscribe()
   }, [])
+
+  useEffect(() => {
+    if(session){
+      document.querySelector('.all-messages').scrollTop = document.querySelector('.all-messages').scrollHeight;
+    }
+  }, [messages])
 
   if (!session) {
     return (
@@ -108,19 +138,44 @@ const App = () => {
   }
   else {
     return (
+
       <div>
-        <ul className='text-light'>
-          {messages.map((message, index) => (
-            <li key={index}>
-              <p>{message.sender} : {message.message}</p>
-            </li>
-          ))}
-        </ul>
-        <form action="" onSubmit={addMessage}>
-          <input type="text" className='msgbox' onChange={(e) => { setNewMessage(e.target.value); }} />
-          <button type='submit'>Send</button>
-        </form>
-        <button onClick={signOut}>Sign out</button>
+        <button onClick={signOut} className='btn btn-danger fs-2'>Sign out</button>
+        <h3 className='text-light'>Welcome {session?.user?.email}</h3>
+
+        <div className="messages-box text-light row m-0 d-flex justify-content-center">
+          <div className="col-12 col-md-4 col-xl-3" style={{ height: "80vh", overflowY: "scroll", background: "#021526" }}>
+            <div className="users-list">
+              <h4>Users</h4>
+              <div className='all-users list-unstyled'>
+                <div onClick={()=>{setSelectedUser('public'); setSelectedClass('publicchatsecbtn')}} id='publicchatsecbtn' className='ps-2 d-flex my-2 align-items-center rounded-5 selected' style={{ height: "50px", width: "100%", background: "#03346E" }}>Public</div>
+                {allUsers.map((user, index) => (
+                  <div id={user.id} onClick={()=>{setSelectedUser(user.email); setSelectedClass(user.id);}} className='d-flex my-2 align-items-center rounded-5' style={{ height: "50px", width: "100%", background: "#03346E" }} key={index}>
+                    <p className='ps-2'>{user.email}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-7 col-xl-8 msgsec" style={{ height: "80vh", background: "#021526" }}>
+            <div className="messages">
+              <h4>Messages</h4>
+              <div className='all-messages' style={{ height: "70vh", overflowY: "scroll" }}>
+                {messages.filter(item=>(item.sender === session.user.email && item.to === selectedUser) || (item.sender === selectedUser && item.to === session.user.email) || (item.to === 'public' && selectedUser==='public')).map((message, index) => (
+                  <div className={`d-flex my-2 align-items-center py-1 rounded-5 ${message.sender===session.user.email?"selected":""}`} style={{ width: "100%", background: "#03346E" }} key={index}>
+                    <p className='ps-2'>{message.sender} : {message.message}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <form action="" className='sendmsgform' onSubmit={addMessage}>
+              <input type="text" className='msgbox' onChange={(e) => { setNewMessage(e.target.value); }} />
+              <button type='submit' className='btn btn-dark'>Send</button>
+            </form>
+          </div>
+        </div>
+
+
       </div>
     )
   }
--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const serviceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzdW54bHB1am14eGpzb3NkZ3NjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTExNzg2NiwiZXhwIjoyMDU2NjkzODY2fQ.Z9vE_osVhF5NL-ENYP1Qk6EMEnB5dSi6pnwpFeThCX0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabaseAdmin = createClient(supabaseUrl, serviceKey, {  
+    auth: {  
+      autoRefreshToken: false,  
+      persistSession: false  
+    }  
+  }); 
 
 export default supabase
