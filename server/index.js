223db985e2680e05e5b5a6c1e0e14ba5e4cdba1b
Yusif Abbaszade <yusif@yusif.yusif>
2025-03-11 01:30:19 -0400

    SA

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import serverless from 'serverless-http';
-import encryptMessage from '../modules/encrypt.js';
-import decryptMessage from '../modules/decrypt.js';
-import supabaseAdmin from '../modules/supabaseAdmin.js';
+import encryptMessage from './modules/encrypt.js';
+import decryptMessage from './modules/decrypt.js';
+import supabaseAdmin from './modules/supabaseAdmin.js';
 
 dotenv.config();
 
