57ad037d30c4b303beb3230ed1cc71b72f3c6d89
Yusif Abbaszade <yusif@yusif.yusif>
2025-03-11 15:02:15 -0400

    SOME_FIXES

--- server/modules/CodeDecode.js
+++ server/modules/CodeDecode.js
@@ -1,22 +1,4 @@
-// import checkXOR from './XOR.js';
 import pidata from '../utils/pidata.js';
-
-// const encryptMessage = (message) => {
-//     let encryptedMessage = '';
-//     for (let i = 0; i < message.length; i++) {
-//         let xor = checkXOR(parseInt(message.charCodeAt(i)), parseInt(pidata.charAt(i)));
-//         if (xor === 1) {
-//             var index = parseInt(message.charCodeAt(i)) + parseInt(pidata.charAt(i));
-//         } else {
-//             var index = parseInt(message.charCodeAt(i)) - parseInt(pidata.charAt(i));
-//         }
-//         encryptedMessage = encryptedMessage + String.fromCharCode(index);
-//     }
-//     return encryptedMessage;
-// }
-
-
-
 
 function decimalToBinary(decimal) {
     return decimal.toString(2);
