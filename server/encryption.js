66f461064ef2e0df847dab29134b715460bb73ea
Yusif Abbaszade <yusif@yusif.yusif>
2025-04-06 20:58:35 -0400

    SALA

--- server/encryption.js
+++ server/encryption.js
@@ -3,8 +3,7 @@
 // const piDigits = mpmath.mp.pi.toString().slice(2); // Get digits of π after the decimal point
 const piDigits = '3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648';
 
-
-const OFFSET = 65535; // Add an offset to ensure u_new is always positive
+ // Add an offset to ensure u_new is always positive
 
 function encrypt(text) {
     let result = "";
@@ -19,17 +18,17 @@ function encrypt(text) {
 
         let u_new, marker;
         if (i % 2 === 0 && p % 2 === 0) {
-            u_new = u + i + p + OFFSET; // Add OFFSET to ensure u_new is positive
+            u_new = u + i + p ; // Add OFFSET to ensure u_new is positive
             marker = '1';
         } else if (i % 2 === 0 && p % 2 === 1) {
-            u_new = u + i - p + OFFSET; // Add OFFSET
+            u_new = u + i - p ; // Add OFFSET
             marker = '2';
         } else if (i % 2 === 1 && p % 2 === 1) {
-            u_new = u - i - p + OFFSET; // Add OFFSET
+            u_new = u - i - p ; // Add OFFSET
             marker = '3';
         } else {
             // Odd index, even `p`
-            u_new = u - i + p + OFFSET; // Add OFFSET
+            u_new = u - i + p ; // Add OFFSET
             marker = '4';
         }
 
@@ -44,7 +43,7 @@ function decrypt(text) {
     while (i < text.length) {
         const char = text[i];
         const marker = text[i + 1];
-        const u_new = char.charCodeAt(0) - OFFSET; // Subtract OFFSET to restore the original value
+        const u_new = char.charCodeAt(0) ; // Subtract OFFSET to restore the original value
 
         const i_index = result.length; // Correctly calculate the index based on the decrypted result length
 
